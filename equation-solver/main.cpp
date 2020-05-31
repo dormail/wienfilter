3bdc4eb2baa777663b4a3a5058a6bd6f09ed3c35
Matthias Maile <matthias.maile@tu-dortmund.de>
2020-05-31 08:56:29 +0200

    variables export added

--- equation-solver/main.cpp
+++ equation-solver/main.cpp
@@ -2,6 +2,7 @@
 #include "functions.h"
 #include <tuple>
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[]){
 	std::tuple<double, double> r0 (0,0);
@@ -10,11 +11,25 @@ int main(int argc, char* argv[]){
 	double T = 10;
 	double dt = 0.001;
 	double B = 1;
-	double E = -2;
+	double E = 2;
 	double m = 1;
 	double q = 1;
 	
 	move(r0, v0, E, B, T, dt, m, q);
+
+	// outputting variables in a file
+	std::ofstream output;
+	output.open("variables.csv");
+
+	output << "T,dt,B,E,m,q\n";
+	output << T << ',';
+	output << dt << ',';
+	output << B << ',';
+	output << E << ',';
+	output << m << ',';
+	output << q;
+
+	output.close();
 
 	return 0;
 }
