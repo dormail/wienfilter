53504b0da2f2b706802b88c03d11576dca02cc46
Matthias Maile <matthias.maile@tu-dortmund.de>
2020-05-31 07:24:58 +0200

    export function added

--- equation-solver/functions.h
+++ equation-solver/functions.h
@@ -2,6 +2,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 #include <tuple>
+#include <vector>
 
 std::tuple<double, double> step(std::tuple<double, double> r, std::tuple<double, double> v, double dt);
 
@@ -9,6 +10,8 @@ std::tuple<double, double> step(std::tuple<double, double> r, std::tuple<double,
 std::tuple<double, double> lorentzforce(std::tuple<double, double> v, double B);
 
 void move(std::tuple<double, double> x0, std::tuple<double, double> v0, double E, double B, double T, double dt, double m);
+
+void make_csv(std::vector<double> d1, std::vector<double> d2, std::vector<double> d3, std::vector<double> d4, std::vector<double> d5);
 
 #endif 
 /*** functions.h end ***/
